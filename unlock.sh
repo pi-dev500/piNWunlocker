43755baff1c506f013aa4625e9afdb9503728a8a
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2024-04-02 14:45:24 +0200

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -8,6 +8,28 @@ sudo n lts
 sudo xpm install --global @xpack-dev-tools/openocd@latest
 }
 function unlock {
+# create directory
+cd "$HOME"
+mkdir -p Nunlock
+cd Nunlock
+# setup confiles
+cfg="source [find interface/sysfsgpio-raspberrypi.cfg]
+
+transport select swd
+
+source [find target/stm32f7x.cfg]
+
+reset_config none separate"
+
+dfuscr="mww 0x40023C08 0x08192A3B
+mww 0x40023C08 0x4C5D6E7F
+mww 0x40023C18 0x00400080
+mww 0x40023C14 0xC0FFAAFE"
+echo "$cfg">n0110.cfg
+echo "$dfuscr">forcedfu.run
+
+# unlock
+openocd -f "n0110.cfg" -c "init" -f "forcedfu.run"
 }
 if [ ! -v openocd ];then
 install_ocd
