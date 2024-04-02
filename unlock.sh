7c6c15c46fbc54a5ce5b8548c5548c4a3aa4ba06
pi-dev500 <77228126+pi-dev500@users.noreply.github.com>
2024-04-02 14:24:00 +0200

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 function install_ocd {
+# for others releases than bookworm
+curl -fsSL https://deb.nodesource.com/install_20.x | sudo bash -
+sudo apt install nodejs
+sudo npm install --global -y n xpm@latest
+sudo n lts
+sudo xpm install --global @xpack-dev-tools/openocd@latest
 }
 if [ ! -v openocd ];then
 install_ocd
